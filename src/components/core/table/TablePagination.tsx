5d4214d59530aed36389328a4cad13e98f37cd6a
김민정 <minj@medistaff.co.kr>
2024-11-18 05:34:37 +0000

    Merged in feature/WEB-315-sms-list-pagination (pull request #94)
    fix: [페이지네이션] 간헐적으로 페이지네이션 동작이 원활하지 않은 부분 수정
    
    * fix: [페이지네이션] 간헐적으로 페이지네이션 동작이 원활하지 않은 부분 수정

--- src/components/core/table/TablePagination.tsx
+++ src/components/core/table/TablePagination.tsx
@@ -37,7 +37,10 @@ const TablePagination = <T extends object>({
   const onPageChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>, _page: number) => {
       let page = event.target.value;
-      if (event.target.value === undefined) page = _page;
+      // 이 조건이 있으면 event.target.value가 ''이 되는 시점에 페이지네이션이 제대로 동작하지 않음
+      // 이 조건이 왜 있는지, event.target.value가 ''이 되는 시점은 언제인지 명확하지 않아서 주석처리로 남김
+      // if (event.target.value === undefined)
+      page = _page;
       logger.debug('<onPageChange> page : ', page);
       setCSearchParamsFunc({ currentPage: page || 0 });
     },
