31ea7bcb213b64fc8f77052afa3f2653a0a5ddcc
Minjeong <minj@medistaff.co.kr>
2025-05-15 18:18:43 +0900

    feat: 등록회원 리스트 가져올때 수동계좌이체 컴포넌트 삭제

--- src/app/(afterLogin)/[confStringIdx]/user/attendee/register/list/page.tsx
+++ src/app/(afterLogin)/[confStringIdx]/user/attendee/register/list/page.tsx
@@ -3,14 +3,14 @@
 import { RegisterAttendeeListTypeToss } from '@/app/(afterLogin)/[confStringIdx]/user/attendee/register/list/RegisterAttendeeListTypeToss';
 import { Pco } from '@/redux/slices/pcoSlice';
 import { useAppSelector } from '@/redux/hooks';
-import { RegisterAttendeeListTypeManual } from '@/app/(afterLogin)/[confStringIdx]/user/attendee/register/list/RegisterAttendeeListTypeManual';
 
 const RegisterAttendeeList = () => {
   const pco: Pco = useAppSelector((state) => state.pco);
   if (pco.paymentMethod === 'toss_payment') {
     return <RegisterAttendeeListTypeToss />;
   } else {
-    return <RegisterAttendeeListTypeManual />;
+    // return <RegisterAttendeeListTypeManual />;
+    return null;
   }
 };
 
